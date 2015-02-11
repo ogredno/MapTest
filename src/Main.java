a29202ec579c7845360cefeab772093e87be89b8
OGREdno <zizubest@meta.ua>
2015-02-11 22:49:32 +0200

    ystranil bag

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,24 @@
 import java.util.*;
-public class Main {
-    public static void main(String args[]){
-        MapTest maptest=new MapTest();
-        Scanner scanner=new Scanner(System.in);
 
-        while(true){
+public class Main {
+    public static void main(String args[]) {
+        MapTest maptest = new MapTest();
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
             maptest.menu();
             int choise = scanner.nextInt();
-            if(choise<=0 || choise>3)break;
-            switch (choise){
-                case 1:{
-                   maptest.setMap(maptest.setFio(),maptest.setBal());
+            if (choise <= 0 || choise > 3) break;
+            switch (choise) {
+                case 1: {
+                    maptest.setMap(maptest.setFio(), maptest.setBal());
                     break;
                 }
-                case 2:{
+                case 2: {
                     maptest.output();
                     break;
                 }
-                case 3:{
+                case 3: {
                     maptest.outputSort();
                     break;
                 }
--- src/MapTest.java
+++ src/MapTest.java
@@ -1,56 +1,55 @@
 
 import java.util.*;
+
 public class MapTest {
     Scanner scanner = new Scanner(System.in);
-    Map<String,String> map=new HashMap<String, String>();
+    Map<String, String> map = new HashMap<String, String>();
 
-    public void menu(){
+    public void menu() {
         System.out.println("1.Input student: ");
         System.out.println("2. Output info: ");
-        System.out.println("2. Output sort: ");
+        System.out.println("3. Output sort: ");
     }
 
-    public String setFio(){
+    public String setFio() {
         System.out.println("Input Surname(space)Surname: ");
-        String name=scanner.nextLine();
+        String name = scanner.nextLine();
         return name;
     }
 
-    public String setBal(){
+    public String setBal() {
         System.out.println("Input bal (b1/b2/b3/b4/b5): ");
-        String bal=scanner.nextLine();
+        String bal = scanner.nextLine();
         return bal;
     }
 
-    public void setMap(String name, String bal){
-        map.put(name,bal);
+    public void setMap(String name, String bal) {
+        map.put(name, bal);
     }
 
-    public void output(){
-        for(Map.Entry<String, String> entry: map.entrySet())
+    public void output() {
+        for (Map.Entry<String, String> entry : map.entrySet())
             System.out.println(entry.getKey() + "  " + entry.getValue());
+    }
+
+    public void outputSort() {
+        for (Map.Entry<String, String> entry : map.entrySet()) {
+            String mas[] = entry.getValue().split("/");
+            double sum = 0;
+            for (int i = 0; i < mas.length; i++) {
+                sum += Integer.valueOf(mas[i]);
             }
-
-    public void outputSort(){
-        int sum=0;
-        int res =0;
-for(String k: map.keySet() ){
-    for(String t: map.values()){
-        String mas[]=t.split("/");
-
-        for(int i=0; i< mas.length; i++){
-            sum +=Integer.valueOf(mas[i]);
+            System.out.println(entry.getKey() + "  " + (sum/5));
         }
-        res=sum/mas.length;
-    }
 
-    System.out.println(k+"    "+res);
 
-}
 
-    }
+
+        }
 
     }
+
+
 
 
 
